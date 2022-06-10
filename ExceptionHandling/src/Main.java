dafea6667de57f54f47ff70a2d2d55ba5d70a270
Ramazan AKSOY <68793314+rmznaksy@users.noreply.github.com>
2022-06-10 23:44:47 +0300

    Add files via upload

---
+++ ExceptionHandling/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+// uygulama çalısırken alınacak hataları önlemek ve programın çalısmasını durdurmayacak hata yönetimi diyebiliriz
+        
+
+
+
+    }
+}
