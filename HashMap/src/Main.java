a169e9b427e5e692084bf14e3618589e5fcf2835
Ramazan AKSOY <68793314+rmznaksy@users.noreply.github.com>
2022-06-11 23:31:08 +0300

    Add files via upload

--- HashMap/src/Main.java
+++ HashMap/src/Main.java
@@ -2,11 +2,11 @@ import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
-
         HashMap<String, String> sozluk = new HashMap<String, String>();
         sozluk.put("book", "kitap");
         sozluk.put("table", "masa");
         sozluk.put("computer", "bilgisayar");
+
         System.out.println("Sözlük Listesi: " + sozluk);
 
         for (String malzeme : sozluk.keySet()) {
@@ -17,6 +17,7 @@ public class Main {
         veri.put(1, "Ahmet");
         veri.put(2, "Ramazan");
         veri.put(3, "Burak");
+
         System.out.println("Veri Listesi: " + veri);
 
         for (Object item : veri.keySet()) {
