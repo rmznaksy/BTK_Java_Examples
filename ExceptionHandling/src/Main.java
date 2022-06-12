879d48a3290e49badd0560aafe4e79ae1f37f694
Ramazan AKSOY <68793314+rmznaksy@users.noreply.github.com>
2022-06-12 13:25:24 +0300

    Add files via upload

--- ExceptionHandling/src/Main.java
+++ ExceptionHandling/src/Main.java
@@ -1,10 +1,18 @@
 /* uygulama çalısırken alınacak hataları önlemek ve 
 programın çalısmasını durdurmayacak hata yönetimi diyebiliriz     */
 
-
 public class Main {
     public static void main(String[] args) {
 
+        try {
+            int[] sayilar= new int[]{1,2,3};
+            System.out.println(sayilar[5]);
+        }catch (Exception exception){
+            System.out.println("Hata oluştu ve Hatanın nedeni:");
+            System.out.println(exception);
+        }finally {
+            System.out.println("Ben her türlü çalışırım...");
+        }
 
 
 
