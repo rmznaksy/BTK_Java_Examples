f8972bf0c4535c12f262fe77268d5bdb2db964ff
Ramazan AKSOY <68793314+rmznaksy@users.noreply.github.com>
2022-06-13 23:32:03 +0300

    Add files via upload

--- Generics/src/Main.java
+++ Generics/src/Main.java
@@ -1,8 +1,19 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
 
+        MyList<String> sehirler = new MyList<>();
+        sehirler.add("Ankara");
+        sehirler.add("Yozgat");
+/* ---------------------------------------------------------------------------------*/
+        MyList<Customer> customerMyList = new MyList<>();
+        customerMyList.add(new Customer());
+        customerMyList.remove(new Customer());
 
 
-        
+
+
+
     }
 }
