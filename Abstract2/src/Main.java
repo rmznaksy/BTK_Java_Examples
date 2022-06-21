b636c9790e540f90eccf005c16073b6eb7b8bdd9
Ramazan AKSOY <68793314+rmznaksy@users.noreply.github.com>
2022-06-21 23:54:40 +0300

    Add files via upload

--- Abstract2/src/Main.java
+++ Abstract2/src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
+
     public static void main(String[] args) {
+
         CustomerManager customerManager = new CustomerManager();
 //      customerManager.baseDatabaseManager=new MySQLDatabaseManager();
         customerManager.baseDatabaseManager=new OracleDatabaseManager();
