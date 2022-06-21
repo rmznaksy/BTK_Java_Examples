4b213a6340f4fe3ed83f4973d598cae774d7bf39
Ramazan AKSOY <68793314+rmznaksy@users.noreply.github.com>
2022-06-21 23:57:39 +0300

    Add files via upload

--- Inheritance/src/Employee.java
+++ Inheritance/src/Employee.java
@@ -1,14 +1,21 @@
 public class Employee extends Person {
+
     public void List() {
+
         System.out.println("Listelendi");
+
     }
 
     public void Add() {
+
         System.out.println("Eklendi");
+
     }
 
     public void BestEmployee() {
+
         System.out.println("Ayın elemanı getirildi.");
+
     }
 
 
